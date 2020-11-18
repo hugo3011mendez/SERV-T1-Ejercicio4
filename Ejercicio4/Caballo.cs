1f6b1c6ca4619e2f4b4b296c283d1c9b21e3bc28
Hugo Mendez <hugo3011mendez@gmail.com>
2020-11-18 18:56:10 +0100

    - Añadido control de errores en el Main.
    - Añadida clase Caballo con propiedad posicion y función correr.
    
    - Añadida estructura básica del Main y de la funcion que deben realizar los caballos.
    
    - Pendiente mostrar caballo ganador

---
+++ Ejercicio4/Caballo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Caballo
+    {
+        public int Posicion { get; set; }
+
+        public void correr()
+        {
+            Random generador = new Random();
+            
+            this.Posicion += generador.Next(1, 101);
+        }
+
+        public Caballo()
+        {
+            this.Posicion = 0;
+        }
+    }
+}
--- Ejercicio4/Ejercicio.cs
+++ Ejercicio4/Ejercicio.cs
@@ -9,7 +9,37 @@ namespace Ejercicio4
 {
     class Ejercicio
     {
-        static Thread[] caballos = new Thread[5]; // Creo el array de 5 caballos
+        static Caballo[] caballos = new Caballo[5]; // Creo el array de 5 caballos
+        static int meta = 400; // Creo y declaro la variable meta para determinar a cuántos metros está la meta
+
+        static bool flag = false; // Creo y declaro el flag
+        static readonly object l = new object(); // Creo el objeto del lock, porque los hilos están usando recursos iguales
+
+
+        // Aquí hago la función que van a realizar los hilos de los caballos
+        static void accionesCaballo(Caballo caballo)
+        {
+            lock (l)
+            {
+                if (!flag)
+                {
+
+                    while (!flag)
+                    {
+                        Random generador = new Random();
+                        int dormir = generador.Next(100, 5000);
+
+                        caballo.correr(); // Llamo a la función correr para ver cuánta distancia recorre el caballo
+                        Thread.Sleep(dormir);
+
+                        if (caballo.Posicion >= meta)
+                        {
+                            flag = true;
+                        }
+                    }
+                } 
+            }
+        }
 
         static void Main(string[] args)
         {
@@ -17,6 +47,12 @@ namespace Ejercicio4
             {
                 Console.WriteLine("Por qué caballo apuestas?");
                 int apuesta = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < caballos.Length; i++)
+                {
+                    caballos[i] = new Caballo();
+
+                }
 
             }
             catch (Exception ex)
